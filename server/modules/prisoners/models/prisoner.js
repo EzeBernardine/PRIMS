cedd50bec2b0f96cdace271dc2b53ae29b6aaf8a
Ezekiel <ezekielblanc1@gmail.com>
2019-10-23 14:35:01 +0100

    setup types for prisoners

---
+++ server/modules/prisoners/models/prisoner.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const prisonerSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  nationality: {
+    type: String,
+    required: true
+  },
+  state: {
+    type: String,
+    required: true
+  },
+  DateOfEntry: {
+    type: Date.now(),
+    required: true
+  },
+  timeOfEntry: {
+    type: String,
+    required: true
+  },
+  LGA: {
+    type: String,
+    required: true
+  },
+  story: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true
+  }
+})
+
+const Prisoner = mongoose.model('prisoner', prisonerSchema);
+
+module.exports = Prisoner
